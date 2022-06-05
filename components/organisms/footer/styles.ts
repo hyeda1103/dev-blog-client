3324502d470c0112464f2c8a6c849b1f90512e23
hyeda1103 <dalgona92@gmail.com>
2022-06-06 01:12:58 +0900

    fix: footer

--- components/organisms/footer/styles.ts
+++ components/organisms/footer/styles.ts
@@ -6,16 +6,13 @@ export const Root = styled.footer`
   width: 100%;
   padding: 8px 0;
   bottom: 0;
-  border-top: 1px solid ${({ theme }) => theme.text};
 `
 
 export const Inner = styled.div`
   position: relative;
   width: 840px;
   margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  text-align: center;
 `
 
 export const Copyright = styled.p`
