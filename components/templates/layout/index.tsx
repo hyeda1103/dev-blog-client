0af26e98e36c1d312a12c7b067f959dea401a925
hyeda1103 <dalgona92@gmail.com>
2022-06-15 18:49:15 +0900

    feat: favicon

--- components/templates/layout/index.tsx
+++ components/templates/layout/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Script from 'next/script'
 import Router, { useRouter } from 'next/router'
+import { ReactNode } from 'react'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
@@ -9,17 +10,14 @@ import Footer from '@root/components/organisms/footer'
 import { Main } from './styles'
 import * as T from '@root/types'
 interface Props {
-  title?: string
-  keywords?: string
-  description?: string
-  children: React.ReactNode
+  children: ReactNode
 }
 
 Router.events.on("routeChangeStart", NProgress.start);
 Router.events.on("routeChangeError", NProgress.done);
 Router.events.on("routeChangeComplete", NProgress.done);
 
-export default function Layout({ title, keywords, description, children }: Props) {
+export default function Layout({ children }: Props) {
   const router = useRouter()
   const IsPosting = router.asPath === T.Page.POST
   const onAdminPage = router.asPath.startsWith(T.Page.ADMIN)
@@ -27,9 +25,7 @@ export default function Layout({ title, keywords, description, children }: Props
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta name="keywords" content={keywords} />
-        <meta name="description" content={description} />
+        <title>해다코의 블로그</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css" />
       </Head>
